ed832f3c0f656f15a9594b9d8661a4f2a0b29030
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-04-23 22:27:36 +0300

    feat: add mongoDB to insert transactions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 connectDB();
 app.use("/api", stkPushRouter);
